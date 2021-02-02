b4cd8eff533cdd67ff77e6b633dc98b01beb1335
Marta <martaslysz93@gmail.com>
2021-02-02 23:03:34 +0100

    Initial commit

---
+++ src/main/java/com/technicaltask/slmarta/SlmartaApplication.java
@@ -0,0 +1,13 @@
+package com.technicaltask.slmarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmartaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmartaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technicaltask/slmarta/SlmartaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicaltask.slmarta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmartaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
