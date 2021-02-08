3dc6f3e08e6d6e7735d3c0a113554472f394636e
Marta <martaslysz93@gmail.com>
2021-02-08 22:24:49 +0100

    replaces @AllowFiltering annotation with queries

--- src/main/java/com/technicaltask/slmarta/SlmartaApplication.java
+++ src/main/java/com/technicaltask/slmarta/SlmartaApplication.java
@@ -1,22 +1,10 @@
 package com.technicaltask.slmarta;
 
-import com.technicaltask.slmarta.message.Message;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
-
 @SpringBootApplication
 public class SlmartaApplication {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SlmartaApplication.class);
-
-    private static Message newMessage(String email, String title, String content, int magicNumber){
-        return new Message(UUID.randomUUID().toString(), email, title, content, magicNumber);
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(SlmartaApplication.class, args);
--- src/main/java/com/technicaltask/slmarta/message/MessageRepository.java
+++ src/main/java/com/technicaltask/slmarta/message/MessageRepository.java
@@ -1,14 +1,15 @@
 package com.technicaltask.slmarta.message;
 
-import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MessageRepository extends CassandraRepository<Message, String> {
 
-    @AllowFiltering
-    List<Message> findAllByMagicNumber(int number);
-    @AllowFiltering
-    List<Message> findAllByEmail(String email);
+    @Query("SELECT * FROM messages WHERE magicNumber=:number ALLOW FILTERING")
+    List<Message> findAllByMagicNumber(@Param("number") int number);
+    @Query("SELECT * FROM messages WHERE email=:email ALLOW FILTERING")
+    List<Message> findAllByEmail(@Param("email") String email);
 }
